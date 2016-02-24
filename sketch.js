d1b984725a46d8fd4c3df53b485a9cfb64c9c0d0
shelrobin <shelbyrobin7@gmail.com>
2016-02-23 20:57:36 -0500

    I tried...

--- sketch.js
+++ sketch.js
@@ -41,19 +41,46 @@ var otherData = [
   }
 ];
 
+
+
 function setup() {
-  createCanvas(1000, 500)
-  noStroke()
-  fill("orange")
-  var visTotals = function (someData) {
-    for (var i = 0; i < someData.length; i++){
+  createCanvas(1000, 500);
+  noStroke();
+  fill("orange");
+
+
+var visTotals = function (someData) {
+  for (var i = 0; i < someData.length; i++){
       var barHeight = someData[i].apples + someData[i].orange;
-      rect(i * 15, 0, 10, barHeight * 10)
+      var barWidth=10;
+      var margin = barWidth / 2;
+      var spacing= i * (margin+ barWidth) ;
+      rect(spacing, 0, barWidth, barHeight);
     }
-  }
+}
+
   visTotals(myData);
 }
 
-function draw() {
+var twoItemArrayFromMyData = function (myData) {
+  var finalArray=[];
+  for (var i=0; i < myData.length; i++){
+  var tempArray= [myData[i].apples, myData[i].orange];
+  finalArray.push(tempArray);
+}
+return finalArray;
 
 }
+var sumOfFirstTwoArrayItems = function (anArray) {
+  var finalAray = [];
+  for (var i = 0; i < myData.length; i++){
+    addedArrayItems = someData[i][0]+ someDAta[i][1];
+    finalArray.push(addedArrayItems);
+    
+  }
+  return finalArray;
+}
+
+var fruitTotals = function (myData)[
+  return sumOfFirstTwoArrayItems(twoItemArrayFromMyData(myData))
+  }
